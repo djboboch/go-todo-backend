7d0f1a95da715f1a496a80b806b445f6a9618059
Jakub Chrupek <jakub.chrupek@hotmail.com>
2021-04-18 18:47:37 +0200

    removed unneeded println

--- main.go
+++ main.go
@@ -27,7 +27,6 @@ type Env struct {
 func main() {
 
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	fmt.Println(psqlInfo)
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		log.Fatal(err)
