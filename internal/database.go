b4eff7a8a3454f470e08cae3f3e3bcd31c4565f4
Jakub Chrupek <jakub.chrupek@hotmail.com>
2021-05-11 19:14:23 +0200

    created the database file

---
+++ internal/database.go
@@ -0,0 +1,33 @@
+package internal
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+func createConnectionPool() (*sql.DB, error) {
+	var err error
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	for retryCount := 30; retryCount >= 0; retryCount-- {
+		err = db.Ping()
+		if err != nil {
+			if retryCount == 0 {
+				return nil, fmt.Errorf("not able to establish a connection to the database at %v", psqlInfo)
+			}
+			fmt.Println("Could not connect to DB")
+			time.Sleep(2 * time.Second)
+			continue
+		}
+		break
+	}
+
+	fmt.Println("Successfully connected!")
+
+	return db, nil
+}
